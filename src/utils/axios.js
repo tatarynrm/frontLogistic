4e707654cea5a9fb46117d43209ab797bccc0f42
tatarynrm <tatarynrm@gmail.com>
2023-02-18 15:16:25 +0200

    ok

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://http://3.237.176.222:8800",
+  baseURL: "http://3.237.176.222:8800",
   // baseURL: process.env.REACT_APP_API_URL,
   // withCredentials: true,
   // headers: {
